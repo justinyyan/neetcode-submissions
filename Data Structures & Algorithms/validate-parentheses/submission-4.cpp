592507613e5c935d465a1dca6a7661cf0f689346
justinyyan <justinyyan@gmail.com>
2026-09-13 05:17:38 +0800

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> arse;
+
+        int max = s.size();
+        int tr = 0;
+
+        while(tr < max)
+        {
+            if(s.at(tr) == '(' || s.at(tr) == '[' || s.at(tr) == '{' )
+            {
+                arse.push(s.at(tr));
+
+                tr++;
+                continue;
+            }
+            
+            if(!arse.empty() && (s.at(tr) == arse.top() + 1 || s.at(tr) == arse.top() + 2))
+            {
+                arse.pop();
+
+                tr++;
+                continue;
+            }
+            return false;
+        }
+
+        if(arse.empty())
+        {
+            return true;
+        }
+        return false;
+        
+
+
+    }
+};
